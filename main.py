7e4f95738e5ac67e2d5c7376b889d30b6e06c6d5
Burciaguita <23308060610289@cetis61.edu.mx>
2026-03-19 14:29:46 -0600

    Add icons to username and email fields

--- main.py
+++ main.py
@@ -11,13 +11,22 @@ def main(page: ft.Page):
     contraseña_prueba= "1020"
     correo_prueba= "player124@gmail.com"
 
-    username = ft.TextField(label="username", width=280)
-    correo = ft.TextField(label="correo electronico", width=280)
+    username = ft.TextField(
+       label="username", 
+       width=280,
+       prefix_icon=ft.Icons.FACE
+       )
+    correo = ft.TextField(
+    label="correo electronico", 
+    width=280,
+    prefix_icon=ft.Icons.ALTERNATE_EMAIL
+    )
     contraseña = ft.TextField(
         label="Contraseña",
         password=True,
         can_reveal_password=True,
-        width=280
+        width=280,
+        prefix_icon=ft.Icons.PASSWORD
     )
 
     mensaje = ft.Text("")
@@ -64,7 +73,14 @@ def main(page: ft.Page):
                 ft.ElevatedButton(
                     "iniciar sesion",
                     width=280,
-                    on_click=iniciar_sesion
+                    on_click=iniciar_sesion,
+                  style=ft.ButtonStyle(
+    bgcolor={
+        ft.ControlState.DEFAULT: "#4C7AAF",
+        ft.ControlState.HOVERED: "#3a4498"
+    },
+    color="white"
+)
                 ),
 
                 mensaje
